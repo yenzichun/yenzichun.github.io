50fc31ec051fd7c1e8a4885e2a3dcecfa722b88c
yenzichun <yenping074@gmail.com>
2016-09-17 02:25:28 +0800

    third try
    modify the dropbox link to store the static pictures generated from R

--- _site/build.R
+++ _site/build.R
@@ -18,15 +18,15 @@ local({
   )
   # set where you want to host the figures (I store them in my Dropbox Public
   # folder, and you might prefer putting them in GIT)
-  if (Sys.getenv('USER') == 'yihui') {
+  #if (Sys.getenv('USER') == 'yihui') {
     # these settings are only for myself, and they will not apply to you, but
     # you may want to adapt them to your own website
     knitr::opts_chunk$set(fig.path = sprintf('%s/', gsub('^.+/', '', d)))
     knitr::opts_knit$set(
-      base.dir = '~/Dropbox/Public/jekyll/',
-      base.url = 'http://db.yihui.name/jekyll/'
+      base.dir = 'C:/Users/Yenping/Dropbox/Public/jekyll',
+      base.url = 'https://dl.dropboxusercontent.com/u/59373967/jekyll/'
     )
-  }
+  #}
   knitr::opts_knit$set(width = 70)
   knitr::knit(a[1], a[2], quiet = TRUE, encoding = 'UTF-8', envir = .GlobalEnv)
 })
